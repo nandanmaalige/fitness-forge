eb4b9946d8a02da8acb88b52497f815da00d3fe3
nandanmaalige <nandank.btech23@rvu.edu.in>
2025-04-15 01:18:26 +0530

    init

--- server/index.ts
+++ server/index.ts
@@ -59,12 +59,10 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 5000;
+  const host = process.env.HOST || "localhost";
+  server.listen(port, "localhost", () => {
     log(`serving on port ${port}`);
   });
+  
 })();
