fe02711d5033426829a1892c31469f368a102bde
nandanmaalige <nandank.btech23@rvu.edu.in>
2025-04-15 01:22:16 +0530

    change

--- server/index.ts
+++ server/index.ts
@@ -6,6 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+console.log("Welcome to Fitness Forge!.");
+
 app.use((req, res, next) => {
   const start = Date.now();
   const path = req.path;
